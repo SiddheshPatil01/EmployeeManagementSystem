f7a674960ad90945d70d0ff55dc5a4a5ff8db5ed
Gunjan2619 <68747038+Gunjan2619@users.noreply.github.com>
2024-04-05 06:04:01 -0400

    Updated  IUserServive

--- EMS1/EMS1/Services/IUserServive.cs
+++ EMS1/EMS1/Services/IUserServive.cs
@@ -4,12 +4,30 @@ namespace EMS1.Services
 {
     public interface IUserServive
     {
+        //Admin
         Task<IEnumerable<Users>> getAllAsync();
-        Task<Users> GetById(string id);
-        Task CreateAsync(Users user);
-        Task UpdateAsync(string id,Users user);
-
+        Task<IEnumerable<Users>> getAllManagers();
+        Task<IEnumerable<Users>> getAllEmpBelowManager(string empId);
         Task DeleteAsync(string id);
+ 
+ 
+        //Manager
+ 
+        //Task<IEnumerable<Users>> getAllEmpBelowManager();
+        Task<Users> GetById(string EmpId);
+        Task Update(String id, Users user);
+        Task CreateAsync(Users user);
+ 
+ 
+        //User
+        //Task<Users> GetById(string EmpId);
+        //Task Update(String id, Users user);
+ 
+        //for Authentication
+        //if A user exit with correct password
+ 
+        //Authenticate  
+        Task<bool> Check(string empId, string password, string role);
 
 
     }
